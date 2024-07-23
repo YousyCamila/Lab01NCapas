e7693450396f7ccf9fa851df69589904cf140b90
Camila Guerra <DANNACAMILAGUERRARINCON0@gmail.com>
2024-07-23 15:54:43 -0500

    feat(back-end): Add method CreateAsync to Customers - layer BLL

--- ORDERS/BLL/Customers.cs
+++ ORDERS/BLL/Customers.cs
@@ -1,7 +1,33 @@
-﻿namespace BLL
-{
-    public class Class1
-    {
+﻿using DAL;
+using Entities.Models;
 
+namespace BLL
+{
+    public class Customers
+    {
+        public async Task<Customer> CreateAsync(Customer customer)
+        {
+            Customer customerResult = null;
+            using (var repository = RepositoryFactory.CreateRepository())
+            {
+                // Buscar si el nombre de cliente existe
+                Customer customerSearch = await repository.RetrieveAsync<Customer>(c => c.FirstName == customer.FirstName);
+                if (customerSearch == null)
+                {
+                    // No existe, podemos crearlo
+                    customerResult = await repository.CreateAsync(customer);
+                }
+                else
+                {
+                    // Podríamos aquí lanzar una excepción
+                    // para notificar que el Cliente ya existe.
+                    // Podríamos incluso crear una capa de Excepciones
+                    // personalizadas y consumirla desde otras
+                    // capas.
+                    CustomerExceptions.ThrowCustomerAlreadyExitsException(customerSearch.FirstName, customerSearch.LastName);
+                }
+            }
+            return customerResult!;
+        }
     }
 }
