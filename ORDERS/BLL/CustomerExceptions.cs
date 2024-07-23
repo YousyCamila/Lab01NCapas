265afc29c5d087a5cde9aa4b7187a5a632a05fe2
Camila Guerra <DANNACAMILAGUERRARINCON0@gmail.com>
2024-07-23 15:50:21 -0500

    feat(back-end): Add validations to BLL layer

--- ORDERS/BLL/CustomerExceptions.cs
+++ ORDERS/BLL/CustomerExceptions.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace BLL
 {
-    internal class CustomerExceptions
+    public class CustomerExceptions : Exception
     {
+        private CustomerExceptions(string message) : base(message)
+        {
+
+        }
+
+        public static void ThrowCustomerAlreadyExitsException(string firstName, string lastName)
+        {
+            throw new CustomerExceptions($"Un cliente con ese nombre ya existe{firstName} {lastName}.");
+
+        }
+
+        public static void ThrowInvalidCustomerDataException(string message)
+        {
+
+            throw new CustomerExceptions(message);
+        }
+
+        public static void ThrowInvalidCustomerIdException(int id)
+        {
+            throw new CustomerExceptions($"Cliente con ID {id} no existe.");
+        }
+
+
     }
 }
