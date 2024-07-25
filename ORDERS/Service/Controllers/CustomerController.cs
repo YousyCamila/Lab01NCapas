68a07668e216f1ae972a1c355794b8b5656ae0d3
Camila Guerra <DANNACAMILAGUERRARINCON0@gmail.com>
2024-07-25 10:34:21 -0500

    feat(back-end): Add method CreateAsync to CustomerController.cs

--- ORDERS/Service/Controllers/CustomerController.cs
+++ ORDERS/Service/Controllers/CustomerController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using SCL;
 using System.Linq.Expressions;
 using Microsoft.AspNetCore.Http;
-using BLL.Exceptions;
 
 namespace Services.Controllers
 {
@@ -19,5 +18,46 @@ namespace Services.Controllers
             _bll = bll;
         }
 
+
+        // POST: api/<CustomerController>
+        [HttpPost]
+        public async Task<ActionResult<Customer>> CreateAsync([FromBody] Customer toCreate)
+        {
+            try
+            {
+                var customer = await _bll.CreateAsync(toCreate);
+                return CreatedAtRoute("RetrieveAsync", new { id = customer.Id }, customer); // Use CreatedAtRoute for 201 Created
+            }
+            catch (CustomerExceptions ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                return StatusCode(StatusCodes.Status500InternalServerError, "An unexpected error occurred.");
+            }
+        }
+
+
+        public Task<IActionResult> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<List<Customer>>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<Customer>> RetrieveAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> UpdateAsync(int id, [FromBody] Customer toUpdate)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
