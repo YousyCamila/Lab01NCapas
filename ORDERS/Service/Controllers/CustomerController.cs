8a85b017e6e17b9d25c3cb4d187ac9eb6c88ec8b
Camila Guerra <DANNACAMILAGUERRARINCON0@gmail.com>
2024-07-25 10:24:57 -0500

    feat(back-end): Add Controller-CustomerController.cs

---
+++ ORDERS/Service/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using BLL;
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using SCL;
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Http;
+using BLL.Exceptions;
+
+namespace Services.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase, IService
+    {
+        private readonly Customers _bll; // Dependency injection for better testability
+
+        public CustomerController(Customers bll)
+        {
+            _bll = bll;
+        }
+
+    }
+}
