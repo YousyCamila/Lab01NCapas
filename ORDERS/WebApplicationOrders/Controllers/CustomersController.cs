dc4a349f73ecccb163447ce1d4beb580aeca3f43
Camilla Guerra Rincon <danna_cguerra@soy.sena.edu.co>
2024-07-30 17:06:30 -0500

    feat(back-end): Add CustomerController.cs

---
+++ ORDERS/WebApplicationOrders/Controllers/CustomersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationOrders.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
