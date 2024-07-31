29bba418569e58d387bc7f1cf57804d8a4016c94
Camilla Guerra Rincon <danna_cguerra@soy.sena.edu.co>
2024-07-31 17:16:11 -0500

    feat(back-end): Add view Create to layer WebApplicationOrder

--- ORDERS/WebApplicationOrders/Controllers/CustomersController.cs
+++ ORDERS/WebApplicationOrders/Controllers/CustomersController.cs
@@ -1,14 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using ProxyServer;
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
 
 namespace WebApplicationOrders.Controllers
 {
-    public class CustomersController : Controller
+    public class CustomerController : Controller
     {
         private readonly CustomerProxy _proxy;
 
-        public CustomersController()
+        public CustomerController()
         {
             this._proxy = new CustomerProxy();
 
@@ -19,5 +22,36 @@ namespace WebApplicationOrders.Controllers
             var customers = await _proxy.GetAllAsync();
             return View(customers);
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //POST: Customer/Ceate
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,City,Country,Phone")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var result = await _proxy.CreateAsync(customer);
+                    if (result == null)
+                    {
+                        return RedirectToAction("Error", new { message = "El cliente con el mismo nombre y apellido ya existe " });
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+
+                catch (Exception ex)
+                {
+                    return RedirectToAction("Error", new { message = ex.Message });
+                }
+            }
+            return View(customer);
+        }
     }
 }
+
