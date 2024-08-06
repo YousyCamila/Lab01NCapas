a7fdd963d1cbd243884e4e1f37645c9ef9fb52c5
Camila Guerra <DANNACAMILAGUERRARINCON0@gmail.com>
2024-08-06 10:01:59 -0500

    feat(back-end): Add ProductExepctions.cs to layer BLL

--- ORDERS/BLL/ProductExceptions.cs
+++ ORDERS/BLL/ProductExceptions.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace BLL
 {
-    internal class ProductExceptions
+    public class ProductExceptions : Exception
     {
+        private ProductExceptions(string message) : base(message)
+        {
+        }
+
+        public static void ThrowProductAlreadyExistsException(string productName)
+        {
+            throw new ProductExceptions($"Un producto con el nombre '{productName}' ya existe.");
+        }
+
+        public static void ThrowInvalidProductDataException(string message)
+        {
+            throw new ProductExceptions(message);
+        }
+
+        public static void ThrowInvalidProductIdException(int id)
+        {
+            throw new ProductExceptions($"Producto con ID {id} no existe.");
+        }
     }
 }
